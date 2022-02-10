4f3b800220dd0e52a0cad56b7326835dcbe8d957
Federico Blanco <banco.federicoandres@gmail.com>
2022-02-10 10:27:04 -0300

    avance jue a la mañana

---
+++ ahorcado.js
@@ -0,0 +1,17 @@
+// CREAR X ELEMENTOS LISTA
+let palabras = ["hola"];
+
+const btnIniciarJuego = document.getElementById('iniciar-juego');
+
+
+btnIniciarJuego.addEventListener('click', () =>{
+
+
+
+})
+
+// const letra = document.createElement("li")
+
+// const palabra = getElementById('palabra');
+
+// palabra.appendChild(letra)
