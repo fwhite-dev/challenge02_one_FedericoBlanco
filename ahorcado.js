859f033a56e607a17f9484709ecd453fbad5bd88
Federico Blanco <banco.federicoandres@gmail.com>
2022-02-11 11:06:19 -0300

    avance

--- ahorcado.js
+++ ahorcado.js
@@ -1,34 +1,41 @@
+/* DUDAS 
+!1) ver como llamar a una funcion dentro de un evento
+
+!2) EN EL CODIGO DE ABAJO NO ANDA EL RETURN PERO SI EL CONSOLE.LOG
+
+var palabras = ["hola","chau","perro","gato","devs","oracle"];
+let palabraRamdon = palabras[Math.round(Math.random()*(palabras.length-1))]
+    return palabraRamdon
+    console.log(palabraRamdon)
+
+!3)
+
+*/
 
 const letraIngresada = document.getElementById('input-nueva-palabra');
 
-var palabras = ["hola","chau","perro","gato","devs","oracle"];
 
-const btnIniciarJuego = document.getElementById('iniciar-juego');
-
-// AGREGAR PALABRA
-const btnNuevaPalabra = document.getElementById('nueva-palabra');
-
-btnNuevaPalabra.addEventListener('click',(event)=>{
-    event.preventDefault();
-    return palabras.push(letraIngresada.value)
-})
 
 // SELECCIONAR PALABRA
+var palabras = ["hola","chau","perro","gato","devs","oracle"];
+const btnIniciarJuego = document.getElementById('iniciar-juego');
 
-const selcionarPalabra = () =>{
+btnIniciarJuego.addEventListener('click', () =>{
     let palabraRamdon = palabras[Math.round(Math.random()*(palabras.length-1))]
-    return palabraRamdon
-} 
-
-btnIniciarJuego.addEventListener('click', (event) =>{
-    event.preventDefault();
-    console.log('anda el evento')
-    selcionarPalabra()
-
+    console.log(palabraRamdon)  
 })
 
+// AGREGAR PALABRA
+const btnAgregarPalabra = document.getElementById('nueva-palabra');
+
+btnAgregarPalabra.addEventListener('click',(event)=>{
+    event.preventDefault();
+    palabras.push(letraIngresada.value)
+    
+})
+
+// CREAR LI SEGUN PALABRA
 
 
-// const letra = document.createElement("li")
 
 
