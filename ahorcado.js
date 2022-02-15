bef3bcd1ca2fc2f6eb551416ddc8a579f018d114
Federico Blanco <banco.federicoandres@gmail.com>
2022-02-15 11:35:46 -0300

    01_15/2

--- ahorcado.js
+++ ahorcado.js
@@ -5,47 +5,62 @@
 
 var palabras = ["hola","chau","perro","gato","devs","oracle"];
 let palabraRamdon = palabras[Math.round(Math.random()*(palabras.length-1))]
-    return palabraRamdon
-    console.log(palabraRamdon)
+return palabraRamdon
+console.log(palabraRamdon)
 
 !3) PORQUE EN EL CODIGO NO LLAMA A LA FUNCION palabraArray MIENTRAS Q SI LA LLAMO EN CONSOLA SI FUNCIONA
 
+!4) RETURN FUNCIONA EN ENVENTOS
 */
 
+// AGREGAR PALABRA
+let palabras = ["hola","chau","perro","gato"];
 const letraIngresada = document.getElementById('input-nueva-palabra');
 
-
-
-// SELECCIONAR PALABRA
-let palabras = ["hola","chau","perro","gato"];
-
-let elegirPalabraRandom = () => {
-    return palabras[Math.round(Math.random()*(palabras.length-1))]
-}
-
-// DIVIDIR PALABRA EN SILABAS
-
-let palabraArray = () =>{
-    let arraySilabas = elegirPalabraRandom().split("")
-    return arraySilabas
-}
-
-// CREAR LI SEGUN palabrArray
-
-for (let i of palabraArray()){
-const letra = document.createElement("li")
-letra.textContent=i
-espaciosSilabas.appendChild(letra)
-}
-
-// AGREGAR PALABRA
 const btnAgregarPalabra = document.getElementById('nueva-palabra');
 
 btnAgregarPalabra.addEventListener('click',(event)=>{
     event.preventDefault();
     palabras.push(letraIngresada.value)
+    console.log(palabras)
     
 })
+
+
+// SELECCIONAR PALABRA
+
+const btnInciarJuego = document.getElementById('iniciar-juego')
+// !FUNCION PARA LLAMAR EN EL ENVENTO DEL btnInciarJuego. NO PUEDO LLAMARLA DESDE EL EVENTO
+// let elegirPalabraRandom = () => {
+//     return palabras[Math.round(Math.random()*(palabras.length-1))]
+// }
+
+btnInciarJuego.addEventListener("click", ()=>{
+    console.log("incia el juego")
+    let palabraElegida = palabras[Math.round(Math.random()*(palabras.length-1))]
+    console.log(palabraElegida)
+
+
+    // DIVIDIR PALABRA EN SILABAS
+    
+    let arraySilabas = palabraElegida.split("")
+    console.log(arraySilabas)
+    
+
+
+
+})
+
+
+
+// CREAR LI SEGUN palabrArray
+
+// for (let i of palabraArray()){
+// const letra = document.createElement("li")
+// letra.textContent=i
+// espaciosSilabas.appendChild(letra)
+// }
+
 
 
 
