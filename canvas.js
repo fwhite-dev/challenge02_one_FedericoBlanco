a1bb34efbd055ad00cb4290ec0d30230244b4500
Federico Blanco <banco.federicoandres@gmail.com>
2022-02-16 12:46:55 -0300

    anvance canvas

---
+++ canvas.js
@@ -0,0 +1,13 @@
+// REFERENCIA CANVAS 1200x800
+
+const canvas = document.querySelector('canvas');
+const pincel = canvas.getContext('2d');
+
+pincel.fillStyle = "red"
+pincel.fillRect(0, 0, canvas.width, canvas.height)
+
+
+pincel.fillStyle = "green"
+pincel.fillRect(200,750,15,-600)
+pincel.fillRect(200,150,250,15)
+
